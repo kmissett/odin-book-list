71c174f905d25a5319e8de01e788a7cd240a1768
kmissett <kmissett77@gmail.com>
2022-03-15 08:45:13 -0400

    Updated the Book object to be a class.

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 The Odin project library list.
 
+I updated this from the initial version to use a class for the book object, as per the followup assignment.
+
 TODO:
-1. Lots of CSS styling -- this is the mvp version for now.
-2. Storing myLibrary in localStorage (which I assume is coming).
-3. Filtering by whether the book has been read or not, or by some other criteria
-4. Adding more functionality: maybe put in when you finished a book, if you read it, or looking up cover art, etc. from an API?
+1. Lots of CSS styling, especially for the add book form -- this is the mvp version for now.
+2. I think it might make sense to make the myLibrary array a module as well?
+3. Storing myLibrary in localStorage (which I assume is coming).
+4. Filtering by whether the book has been read or not, or by some other criteria.
+5. Adding more functionality: maybe put in when you finished a book, if you read it, or looking up cover art, etc. from an API?
--- script.js
+++ script.js
@@ -1,69 +1,68 @@
 let myLibrary = []
 
-function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-  this.added = new Date()
-
-  this.isReadText = () => (this.read === true) ? "Already read" : "Want to read"
-
-  this.info = () => `${this.title} by ${this.author}. ${pages} pages. ${this.isReadText}. Added on ${this.added}`
-
-  this.toggleReadStatus = () => this.read = !this.read 
-}
-
-const addBookToLibrary = (book) => {
-
-  // check to make sure the book is not already in the list
-  let isInLibrary = myLibrary.some(prevBook => (prevBook.title === book.title && prevBook.author === book.author))
-  if(isInLibrary) return
-  
-  //set the book ID
-  let bookId = myLibrary.length + 1
-  for (let i=0; i< myLibrary.length; i++) {
-    if(myLibrary[i].id >= bookId) bookId = myLibrary[i].id + 1
+class Book {
+  constructor() {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.added = new Date()
+    this.isReadText = () => (this.read === true) ? "Already read" : "Want to read"
   }
-  book.id = bookId
-
-  myLibrary.push(book)
-}
-
-const createBookCard = (book) => {
-  const template = `
-    <h3 class="book-title">${book.title}</h3>
-    <p class="book-author">${book.author}</p>
-    <p class="book-pages">${book.pages} pages</p>
-    <button class="button read">${book.isReadText()}</button>
-    <button class="button remove">Remove</button>
-  `
-  let newBook = document.createElement("div")
-  newBook.classList.add("book")
-  newBook.dataset.id = (book.hasOwnProperty('id'))? book.id : ""
-  newBook.innerHTML = template
-  main.prepend(newBook)
-  newBook.addEventListener("click", (e) => {
-    const currentCard = e.target.parentNode
-    const currentBookId = parseInt(currentCard.dataset.id)
-    const currentBook = myLibrary.filter(book => book.id === currentBookId)[0]
-
-    if (e.target.classList.contains("remove")) {
+  info = () => `${this.title} by ${this.author}. ${pages} pages. ${this.isReadText}. Added on ${this.added}`
+  toggleReadStatus = () => this.read = !this.read 
   
-      // remove from array
-      myLibrary = myLibrary.filter(book => book.id !== currentBookId)
-  
-      // delete node
-      main.removeChild(currentCard)
+  addBookToLibrary = () => {
+
+    // check to make sure the book is not already in the list
+    let isInLibrary = myLibrary.some(prevBook => (prevBook.title === this.title && prevBook.author === this.author))
+    if(isInLibrary) return
+    
+    //set the book ID
+    let bookId = myLibrary.length + 1
+    for (let i=0; i< myLibrary.length; i++) {
+      if(myLibrary[i].id >= bookId) bookId = myLibrary[i].id + 1
     }
-
-    else if (e.target.classList.contains("read")) {
+    this.id = bookId
+    
+    myLibrary.push(this)
+  }
+  
+    createBookCard = () => {
+    const template = `
+    <h3 class="book-title">${this.title}</h3>
+    <p class="book-author">${this.author}</p>
+    <p class="book-pages">${this.pages} pages</p>
+    <button class="button read">${this.isReadText()}</button>
+    <button class="button remove">Remove</button>
+    `
+    let newBook = document.createElement("div")
+    newBook.classList.add("book")
+    newBook.dataset.id = (this.hasOwnProperty('id')) ? this.id : ""
+    newBook.innerHTML = template
+    main.prepend(newBook)
+    newBook.addEventListener("click", (e) => {
+      const currentCard = e.target.parentNode
+      const currentBookId = parseInt(currentCard.dataset.id)
+      const currentBook = myLibrary.filter(book => book.id === currentBookId)[0]
       
+      if (e.target.classList.contains("remove")) {
+        
+        // remove from array
+        myLibrary = myLibrary.filter(book => book.id !== currentBookId)
+        
+        // delete node
+        main.removeChild(currentCard)
+      }
+      
+      else if (e.target.classList.contains("read")) {
+        
         currentBook.toggleReadStatus()
         e.target.textContent = currentBook.isReadText()
-    }
-    else return
-  }) 
+      }
+      else return
+    }) 
+  }
 }
 
 
@@ -77,15 +76,14 @@ const main = document.querySelector("main")
 
 addBookBtn.addEventListener("click", (e) => {
   e.preventDefault()
-  const bookTitle = addBookForm.title.value
-  const bookAuthor = addBookForm.author.value
-  const bookPages = parseInt(addBookForm.pages.value)
-  const bookIsRead = addBookForm.read.checked
-
-  console.log(bookTitle, bookAuthor, bookPages, bookIsRead)
-  const newBook = new Book(bookTitle, bookAuthor, bookPages, bookIsRead)
-  addBookToLibrary(newBook)
-  createBookCard(newBook)
+  const newBook = new Book()
+  newBook.title = addBookForm.title.value
+  newBook.author = addBookForm.author.value
+  newBook.pages = parseInt(addBookForm.pages.value)
+  newBook.read = addBookForm.read.checked
+  
+  newBook.addBookToLibrary()
+  newBook.createBookCard()
   
   addBookModal.classList.add("display-none")
   addBookForm.reset()
@@ -101,20 +99,41 @@ addBookModal.addEventListener("click", (e) => {
 })
 
 /* add test data to myLibrary */
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true)
-const myths01 = new Book("Mythos", "Stephen Fry", 352, true)
-const myths02 = new Book("Heroes", "Stephen Fry", 352, false)
-const myths03 = new Book("Troy", "Stephen Fry", 288, false)
-addBookToLibrary(theHobbit)
-addBookToLibrary(myths01)
-addBookToLibrary(myths02)
-addBookToLibrary(myths03)
-addBookToLibrary(myths03)
+const theHobbit = new Book()
+theHobbit.title = "The Hobbit"
+theHobbit.author = "J.R.R. Tolkien"
+theHobbit.pages = 295
+theHobbit.checked = true
+
+const myths01 = new Book()
+myths01.title = "Mythos",
+myths01.author =  "Stephen Fry"
+myths01.pages = 352
+myths01.checked = true
+
+const myths02 = new Book()
+myths02.title = "Heroes",
+myths02.author =  "Stephen Fry"
+myths02.pages = 352
+myths02.checked = false
+
+const myths03 = new Book()
+myths03.title = "Troy",
+myths03.author =  "Stephen Fry"
+myths03.pages = 288
+myths03.checked = false
+
+theHobbit.addBookToLibrary()
+myths01.addBookToLibrary()
+myths02.addBookToLibrary()
+myths03.addBookToLibrary()
+myths03.addBookToLibrary()
+
 console.log(myLibrary)
 
 const createBookCards = () => {
   main.innerHTML = ""
-  myLibrary.forEach(book => createBookCard(book))
+  myLibrary.forEach(book => book.createBookCard())
 }
 
 createBookCards()
